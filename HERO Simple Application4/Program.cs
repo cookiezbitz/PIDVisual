9d8927815d3d78d827086f7f5f1594e8ae90a12a
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2023-02-17 17:33:17 +0800

    Added analog input

--- HERO Simple Application4/Program.cs
+++ HERO Simple Application4/Program.cs
@@ -6,25 +6,59 @@ namespace Hero_Simple_Application5
 {
     public class Program
     {
+        //initializes analog inputs
+        static AnalogInput analogInput0 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin3);
+        static AnalogInput analogInput1 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin4);
+        static AnalogInput analogInput2 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin5);
+
         public static void Main()
         {
+            //variable declarations
+            double read0;
+            double read1;
+            double read2;
+
             /* create a gamepad object */
             CTRE.Phoenix.Controller.GameController myGamepad = new CTRE.Phoenix.Controller.GameController(new
            CTRE.Phoenix.UsbHostDevice(1));
             /* create a talon, the Talon Device ID in HERO LifeBoat is zero */
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
+            
+
             /* simple counter to print and watch using the debugger */
             int counter = 0;
             /* loop forever */
             while (true)
             {
+                /* grab analog value */
+                read0 = analogInput0.Read();
+                read1 = analogInput1.Read();
+                read2 = analogInput2.Read();
+
+                /* print the three analog inputs as three columns */
+                Debug.Print("" + read0 + "\t" + read1 + "\t" + read2);
+
+
                 /* added inside the while loop */
                 if (myGamepad.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.Connected)
                 {
+                    // print button value
+                    Debug.Print("button: " + myGamepad.GetButton(1));
                     /* print the axis value */
                     Debug.Print("axis:" + myGamepad.GetAxis(1));
                     /* pass axis value to talon */
-                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, myGamepad.GetAxis(1));
+                   myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput,read0);
+
+                  /*  
+                    if (myGamepad.GetButton(1))
+                    {
+                        myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, 0);
+                        
+                    }
+                  */
+
+
+
                     /* allow motor control */
                     CTRE.Phoenix.Watchdog.Feed();
                 }
