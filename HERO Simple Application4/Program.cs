0b06f0a3f8e54936e0505bdc8e403e9633de409f
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2023-02-18 15:41:21 +0800

    Added PID stuff (integration required)

--- HERO Simple Application4/Program.cs
+++ HERO Simple Application4/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Threading;
+﻿using CTRE.Phoenix;
+using CTRE.Phoenix.Controller;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.MotorControl.CAN;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
+
 namespace Hero_Simple_Application5
 {
     public class Program
@@ -11,6 +14,22 @@ namespace Hero_Simple_Application5
         static AnalogInput analogInput1 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin4);
         static AnalogInput analogInput2 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin5);
 
+        /** talon to control */
+        private TalonSRX _talon = new TalonSRX(1);
+        /** desired mode to put talon in */
+        private ControlMode _mode = ControlMode.PercentOutput;
+        /** attached gamepad to HERO, tested with Logitech F710 */
+        private GameController _gamepad = new GameController(UsbHostDevice.GetInstance());
+        /** constant slot to use */
+        const int kSlotIdx = 0;
+        /** How long to wait for receipt when setting a param.  Many setters take an optional timeout that API will wait for.
+            This is benefical for initial setup (before movement), though typically not desired 
+            when changing parameters concurrently with robot operation (gain scheduling for example).*/
+        const int kTimeoutMs = 30;
+
+
+
+
         public static void Main()
         {
             //variable declarations
@@ -18,6 +37,16 @@ namespace Hero_Simple_Application5
             double read1;
             double read2;
             int forwardbackward = -1;
+            double setpoint;
+            double error;
+            double integral;
+            double derivative;
+            double prevError;
+            double power;
+            double kP;
+            double kI;
+            double kD;
+
 
             /* create a gamepad object */
             CTRE.Phoenix.Controller.GameController myGamepad = new CTRE.Phoenix.Controller.GameController(new
@@ -26,11 +55,55 @@ namespace Hero_Simple_Application5
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
             double encodervalue = (double)myTalon.SetSelectedSensorPosition(0, 0, 0);
 
+            /* Factory Default all hardware to prevent unexpected behaviour */
+            myTalon.ConfigFactoryDefault();
+            /* specify sensor characteristics */
+            myTalon.ConfigSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0);
+            myTalon.SetSensorPhase(false); /* make sure positive motor output means sensor moves in position direction */
+
+
+            /* brake or coast during neutral */
+            myTalon.SetNeutralMode(NeutralMode.Brake);
+
+            /* closed-loop and motion-magic parameters */
+            myTalon.Config_kF(kSlotIdx, 0.1153f, kTimeoutMs); // 8874 native sensor units per 100ms at full motor output (+1023)
+            myTalon.Config_kP(kSlotIdx, 2.00f, kTimeoutMs);
+            myTalon.Config_kI(kSlotIdx, 0f, kTimeoutMs);
+            myTalon.Config_kD(kSlotIdx, 20f, kTimeoutMs);
+            myTalon.Config_IntegralZone(kSlotIdx, 0, kTimeoutMs);
+            myTalon.SelectProfileSlot(kSlotIdx, 0); /* select this slot */
+            myTalon.ConfigNominalOutputForward(0f, kTimeoutMs);
+            myTalon.ConfigNominalOutputReverse(0f, kTimeoutMs);
+            myTalon.ConfigPeakOutputForward(1.0f, kTimeoutMs);
+            myTalon.ConfigPeakOutputReverse(-1.0f, kTimeoutMs);
+            /* Home the relative sensor, 
+    alternatively you can throttle until limit switch,
+    use an absolute signal like CtreMagEncoder_Absolute or analog sensor.
+    */
+            myTalon.SetSelectedSensorPosition(0, kTimeoutMs);
+
+
+
+
             /* simple counter to print and watch using the debugger */
             int counter = 0;
             /* loop forever */
             while (true)
             {
+
+                /*
+                error = setpoint - (double)myTalon.SetSelectedSensorPosition(0, 0, 0);
+                integral = integral + error;
+                if (error = 0 or passes setpoint)
+                    integral = 0;
+                if (error is outside useful range)
+                    integral = 0;
+                derivative = error - prevError;
+                prevError = error;
+                power = error * kP + integral * kI + derivative * kD;
+                */
+
+
                 /* grab analog value */
                 read0 = analogInput0.Read();
                 read1 = analogInput1.Read();
@@ -38,7 +111,7 @@ namespace Hero_Simple_Application5
 
 
                 encodervalue = myTalon.GetSelectedSensorPosition();
-                
+
                 //cannot collect encoder position??????????
 
                 /* print the three analog inputs as three columns */
@@ -50,24 +123,31 @@ namespace Hero_Simple_Application5
                 if (myGamepad.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.Connected)
                 {
                     // print button value
-                    Debug.Print("button: " + myGamepad.GetButton(1));
+                    Debug.Print("button1: " + myGamepad.GetButton(1));
+                    Debug.Print("button2: " + myGamepad.GetButton(2));
                     /* print the axis value */
                     Debug.Print("axis:" + myGamepad.GetAxis(1));
                     /* pass axis value to talon */
                     //-.045 to actually stop the thing as a temporary solution because there is an error regarding that
-                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput,read1 * forwardbackward);
-                   
-                   if(myGamepad.GetButton(1))
+                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, read1 * forwardbackward);
+
+                    if (myGamepad.GetButton(1))
                     {
                         Debug.Print("done here");
                         forwardbackward = -1;
                     }
-                    else
+                    if (myGamepad.GetButton(3))
                     {
                         Debug.Print("done there");
                         forwardbackward = 1;
                     }
+                    
 
+                    
+                    if (myGamepad.GetButton(2))
+                    {
+                        forwardbackward = 0;
+                    }
 
 
 
@@ -81,5 +161,6 @@ Or add it to the Watch Tab */
                 System.Threading.Thread.Sleep(10);
             }
         }
+
     }
 }
