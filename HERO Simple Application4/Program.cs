fbf9a06932f5e1fcf00ac670f26888e41bae7c77
Remiel Lin <74459428+cookiezbitz@users.noreply.github.com>
2023-02-23 21:57:03 +0800

    added custom PID

--- HERO Simple Application4/Program.cs
+++ HERO Simple Application4/Program.cs
@@ -14,6 +14,15 @@ namespace Hero_Simple_Application5
         static AnalogInput analogInput1 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin4);
         static AnalogInput analogInput2 = new AnalogInput(CTRE.HERO.IO.Port8.Analog_Pin5);
 
+
+
+
+
+
+
+
+
+
         /** talon to control */
 
         /** desired mode to put talon in */
@@ -27,11 +36,19 @@ namespace Hero_Simple_Application5
             when changing parameters concurrently with robot operation (gain scheduling for example).*/
         const int kTimeoutMs = 30;
 
+        /* create a gamepad object */
+        CTRE.Phoenix.Controller.GameController myGamepad = new CTRE.Phoenix.Controller.GameController(new
+        CTRE.Phoenix.UsbHostDevice(1));
+        /* create a talon, the Talon Device ID in HERO LifeBoat is zero */
+        CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
 
 
 
         public static void Main()
         {
+
+
+
             //variable declarations
             float read0;
             float read1;
@@ -45,11 +62,30 @@ namespace Hero_Simple_Application5
 
 
 
+            double error = 0;
+            double integral = 0;
+            double derivative = 0;
+            double prevError = 0;
+            double power = 0;
+            double kP = .005;
+            double kI = .005;
+            double kD = .005;
+            double setpoint = 1;
+
+
+
+
+
+
+
+
+
             /* create a gamepad object */
             CTRE.Phoenix.Controller.GameController myGamepad = new CTRE.Phoenix.Controller.GameController(new
             CTRE.Phoenix.UsbHostDevice(1));
             /* create a talon, the Talon Device ID in HERO LifeBoat is zero */
             CTRE.Phoenix.MotorControl.CAN.TalonSRX myTalon = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
+
             double encodervalue = (double)myTalon.SetSelectedSensorPosition(0, 0, 0);
 
             /* Factory Default all hardware to prevent unexpected behaviour */
@@ -88,8 +124,8 @@ namespace Hero_Simple_Application5
 
 
 
-          //  myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.Velocity, 0);
-         //   bollean = false
+            //  myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.Velocity, 0);
+            //   bollean = false
 
 
             //mag encoder units are 4096 counts per rotation
@@ -98,17 +134,19 @@ namespace Hero_Simple_Application5
             /* simple counter to print and watch using the debugger */
             int counter = 0;
             /* loop forever */
+
+
             while (true)
             {
 
 
-
+                /*
                 if (!PIDdone)
                 {
                     myTalon.ConfigPeakOutputForward(runPID, kTimeoutMs);
                     myTalon.ConfigPeakOutputReverse(-runPID, kTimeoutMs);
                     Debug.Print("pid running");
-                    myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.Position, 1);
+
 
                 }
                 if (PIDdone)
@@ -117,6 +155,8 @@ namespace Hero_Simple_Application5
                     myTalon.ConfigPeakOutputForward(stopPID, kTimeoutMs);
                     myTalon.ConfigPeakOutputReverse(-stopPID, kTimeoutMs);
                 }
+                */
+
 
 
 
@@ -129,12 +169,40 @@ namespace Hero_Simple_Application5
 
                 encodervalue = myTalon.GetSelectedSensorPosition();
 
+            
+
+                myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, power);
+
+                while (PIDdone)
+                {
+                    error = setpoint - encodervalue;
+                    integral = integral + error;
+                    if (error == 0 || encodervalue > setpoint)
+                        integral = 0;
+                    if (error > 100)
+                        integral = 0;
+                    derivative = error - prevError;
+                    prevError = error;
+                    power = error * kP + integral * kI + derivative * kD;
+                    Debug.Print("encodervalue: " + encodervalue);
+                 //  Debug.Print("Error: " + error);
+                }
+
+
+
+
+
+
+
+
+
+
                 //cannot collect encoder position??????????
 
                 /* print the three analog inputs as three columns */
-             //   Debug.Print("" + read0 + "\t" + read1 + "\t" + read2);
-               // Debug.Print("Encoder position: " + encodervalue);
-               // Debug.Print("Encoder position in degrees:" + encodervalue / 11.3888888888888888889);
+                //   Debug.Print("" + read0 + "\t" + read1 + "\t" + read2);
+                // Debug.Print("Encoder position: " + encodervalue);
+                // Debug.Print("Encoder position in degrees:" + encodervalue / 11.3888888888888888889);
 
 
                 /* added inside the while loop */
@@ -144,48 +212,57 @@ namespace Hero_Simple_Application5
                     //   Debug.Print("button1: " + myGamepad.GetButton(1));
                     // Debug.Print("button2: " + myGamepad.GetButton(2));
                     /* print the axis value */
-                //     Debug.Print("axis:" + myGamepad.GetAxis(1));
+                    //     Debug.Print("axis:" + myGamepad.GetAxis(1));
                     /* pass axis value to talon */
                     //-.045 to actually stop the thing as a temporary solution because there is an error regarding that
 
                     // myTalon.Set(CTRE.Phoenix.MotorControl.ControlMode.Position, movetoposition);
                     //    
 
-           //         myTalon.Config_kP(kSlotIdx, read0, kTimeoutMs);
-             //       myTalon.Config_kI(kSlotIdx, read1, kTimeoutMs);
-               //     myTalon.Config_kD(kSlotIdx, read2, kTimeoutMs);
+                    //         myTalon.Config_kP(kSlotIdx, read0, kTimeoutMs);
+                    //       myTalon.Config_kI(kSlotIdx, read1, kTimeoutMs);
+                    //     myTalon.Config_kD(kSlotIdx, read2, kTimeoutMs);
 
 
                     if (myGamepad.GetButton(1))
                     {
                         PIDdone = true;
-
+                        Debug.Print("PID true ");
                     }
 
-                    while(currentrotation > 0)
+                    while (myGamepad.GetButton(4))
                     {
-                        Debug.Print("CURRENT ROTATION: " + currentrotation);
+                        Debug.Print("PID FALSE, CURRENT ROTATION: " + currentrotation);
+                        PIDdone = false;
+                        power = 0;
                     }
 
-
+                    /*
                     if (myGamepad.GetButton(3))
                     {
-                        PIDdone = false;
+                        setpoint += .5;
+                        Debug.Print("added to setpoint ");
                     }
-                    
 
-                    
+
+
                     if (myGamepad.GetButton(2))
                     {
-                        forwardbackward = 0;
+                        setpoint += .5;
+                        Debug.Print("removed from setpoint ");
                     }
+                    */
 
+
+                    myTalon.Config_kP(kSlotIdx, read0, kTimeoutMs);
+                    myTalon.Config_kI(kSlotIdx, read1, kTimeoutMs);
+                    myTalon.Config_kD(kSlotIdx, read2, kTimeoutMs);
 
 
                     /* allow motor control */
                     CTRE.Phoenix.Watchdog.Feed();
                 }
-         //       
+                //       
                 /* increment counter */
                 ++counter; /* try to land a breakpoint here and hover over 'counter' to see it's current value.
 Or add it to the Watch Tab */
@@ -195,7 +272,13 @@ Or add it to the Watch Tab */
 
 
 
+
+
         }
 
     }
+
 }
+
+
+    
